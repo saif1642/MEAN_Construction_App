107ef8ef526ffb2ca99cfea17ad15df4151d1a6a
saifdev1642 <saifdev1642@gmail.com>
2018-07-31 23:05:52 +0600

    Node and Angular set up

---
+++ client/amazonoFront/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'amazonoFront';
+  searchTerm = '';
+  isCollapsed = true;
+
+  get token(){
+    return localStorage.getItem('token');
+  }
+  collapse(){
+    this.isCollapsed = true;
+  }
+  closeDropdown(dropdown){
+    dropdown.close();
+  }
+  logout(){
+
+  }
+  search(){
+
+  }
+
+}
---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    database:'mongodb://amazono:amazono12345@ds023303.mlab.com:23303/amazono',
+    port:'3000',
+    secret:'Amazonosecret123456987'
+}
